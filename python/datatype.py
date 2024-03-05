5ac6ec20bb9afc7bf0650ae15649419f4c65cbfd
Piyush-Pant2023 <149172283+Piyush-Pant2023@users.noreply.github.com>
2024-03-05 19:12:53 +0530

    Add files via upload

---
+++ python/datatype.py
@@ -0,0 +1,16 @@
+a=10
+print(type(a))
+b=15.25
+print(type(b))
+c=5+6j
+print(type(c))
+f=("pyush","pp","pnt")
+print(type(f))
+g="p"
+print(type(g))
+h=True
+print(type(h))
+i={"apple","banana"}
+print(type(i))
+u=None
+print(type(u))
